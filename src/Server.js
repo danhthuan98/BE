15e4963a9607ab8abe826f4365f7f9ac31d2d6b7
thinhtd <thinhtd6758@seta-international.vn>
2022-10-19 00:02:23 +0700

    upload more picture

--- src/Server.js
+++ src/Server.js
@@ -35,7 +35,7 @@ mongoose.connect(`mongodb+srv://${process.env.MONGO_DB_USER}:${process.env.MONGO
 });
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
 app.use('/public', express.static(path.join(__dirname, 'uploads')));
 app.use('/api', authRouter);
 app.use('/api', categoryRouter);
